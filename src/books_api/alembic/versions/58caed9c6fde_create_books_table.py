4de846185e896c3264b10520696da0fcda2bcd18
Yuri Perim <yuriperim@gmail.com>
2025-11-20 18:01:40 -0300

    feat: adiciona script para banco de dados local e primeira migration

---
+++ src/books_api/alembic/versions/58caed9c6fde_create_books_table.py
@@ -0,0 +1,37 @@
+"""create books table
+
+Revision ID: 58caed9c6fde
+Revises:
+Create Date: 2025-11-20 12:36:49.333008
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = "58caed9c6fde"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "books",
+        sa.Column("titulo", sa.String(50), primary_key=True),
+        sa.Column("preco", sa.Numeric(10, 2), nullable=False),
+        sa.Column("rating", sa.Integer, nullable=False),
+        sa.Column("disponibilidade", sa.Integer, nullable=False),
+        sa.Column("categoria", sa.String(25), nullable=False),
+        sa.Column("url_imagem", sa.String(), nullable=False),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("books")
