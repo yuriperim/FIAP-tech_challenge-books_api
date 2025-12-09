92e6865d2df82ba9003729219a5504b5418c5317
Yuri Perim <yuriperim@gmail.com>
2025-12-08 21:50:54 -0300

    feat: implementa função de transformação do ETL

--- src/books_api/services/books_etl.py
+++ src/books_api/services/books_etl.py
@@ -1,11 +1,13 @@
+import re
 import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 
 
-def extract_books() -> list[dict]:
-    BASE_URL = "https://books.toscrape.com"
+BASE_URL = "https://books.toscrape.com"
 
+
+def extract_books() -> list[dict]:
     books_raw = []
 
     CONTINUE = True
@@ -56,6 +58,42 @@ def extract_books() -> list[dict]:
 
 def transform_books(books_raw: list[dict]) -> list[dict]:
     books_transformed = []
+
+    book_id_pattern = re.compile(r"(?P<book_id>\d*)/index.html$")
+    book_availability_pattern = re.compile(r"(?P<book_availability>\d*)\s+available")
+
+    ratings_map = {
+        "One": 1,
+        "Two": 2,
+        "Three": 3,
+        "Four": 4,
+        "Five": 5,
+    }
+
+    for book_raw in books_raw:
+        book_id = int(
+            book_id_pattern
+            .search(book_raw["book_id"])
+            .group("book_id")
+        )
+        book_price = float(book_raw["book_price"][2:])
+        book_rating = ratings_map.get(book_raw["book_rating"], 0)
+        book_image = f'{BASE_URL}/{book_raw["book_image"][3:]}'
+        book_availability = int(
+            book_availability_pattern
+            .search(book_raw["book_availability"])
+            .group("book_availability")
+        )
+
+        books_transformed.append({
+            "book_id": book_id,
+            "book_title": book_raw["book_title"],
+            "book_price": book_price,
+            "book_rating": book_rating,
+            "book_image": book_image,
+            "book_availability": book_availability,
+            "book_category": book_raw["book_category"],
+        })
 
     return books_transformed
 
